86596294812816ea0d0cb71b5802819807123744
Seokki Kwon <kwons4163@naver.com>
2023-12-10 23:27:09 +0900

    fix: 상세페이지 들어갔다 나올때 탭바가 잠시 사라지는현상

---
+++ TrackUs/TrackUs/Sources/Main/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  TrackUs
+//
+//  Created by 박선구 on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var tabBarIndex: Int = 0
+    
+    var body: some View {
+        NavigationView {
+            TabView(selection: $tabBarIndex) {
+                MainView()
+                    .onTapGesture {
+                        tabBarIndex = 0
+                    }
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("홈")
+                    }.tag(0)
+                
+                MateView()
+                    .onTapGesture {
+                        tabBarIndex = 1
+                    }
+                    .tabItem {
+                        Image(systemName: "doc.plaintext")
+                        Text("메이트 모집")
+                    }.tag(1)
+                
+                CommunityView()
+                    .onTapGesture {
+                        tabBarIndex = 2
+                    }
+                    .tabItem {
+                        Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
+                        Text("커뮤니티")
+                    }.tag(2)
+                
+                MyPageView()
+                    .onTapGesture {
+                        tabBarIndex = 3
+                    }
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("내 정보")
+                    }.tag(3)
+            }
+            .navigationTitle(tabTitle)
+            .navigationBarTitleDisplayMode(.inline)
+            .accentColor(.sub)
+        }
+    }
+    
+    var tabTitle: String {
+        switch tabBarIndex {
+        case 0: return "홈"
+        case 1: return "메이트 모집"
+        case 2: return "커뮤니티"
+        case 3: return "내 정보"
+        default: return "홈"
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
