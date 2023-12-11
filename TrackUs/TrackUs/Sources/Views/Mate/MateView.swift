1545950b75d0e4e15b3f6bf45bd153cd60bee409
= <=>
2023-12-11 11:47:52 +0900

    fix: 화면이동 수정

--- TrackUs/TrackUs/Sources/Views/Mate/MateView.swift
+++ TrackUs/TrackUs/Sources/Views/Mate/MateView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct MateView: View {
     @State private var action: Int? = 0  // 화면이동 테스트를 위해서 임시로 선언
     var body: some View {
-        Text("메이트 모집")
-        NavigationStack {
+
             ZStack(alignment: .bottomTrailing) {
                 Color.main
                     .edgesIgnoringSafeArea(.all)
@@ -24,7 +23,6 @@ struct MateView: View {
                 .navigationBarTitleDisplayMode(.inline)
                 .padding(.horizontal, 20)
                 
-            }
         }
     }
 }
