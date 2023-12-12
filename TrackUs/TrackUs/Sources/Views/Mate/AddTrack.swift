1415282ac58f6b97447a17300d7a60752d5794c5
kwonseokki <kwons4163@naver.com>
2023-12-12 15:01:03 +0900

    design: 코스등록 화면 레이아웃 수정 - 텍스트필드 중앙간격 안맞는 문제수정 - 키보드와 등록 버튼이 같이 올라오도록 수정 - 화면 터치할때 키보드 내려가도록 수정

--- TrackUs/TrackUs/Sources/Views/Mate/AddTrack.swift
+++ TrackUs/TrackUs/Sources/Views/Mate/AddTrack.swift
@@ -8,213 +8,201 @@
 import SwiftUI
 
 struct AddTrack: View {
+    @State var trackName : String = "" // 트랙이름
+    @State var limitedMember : Int = 1 // 제한인원
+    @State var estimatedTime: Int = 0 // 예상시간(분)
+    @State var estimatedDistance : Double = 0 // 예상거리
+    @State var calories : Int = 0 // 소모칼로리
+    @State var startDate = Date() // 시작날짜
+    @State var trackBio : String = "" // 트랙소개
     
-    @State var numberOfPerson : Int = 1
-    
-    @State var estimatedTime: Int = 0
-    @State var estimatedDistance : Double = 0
-    @State var calories : Int = 0
-    
-    @State var runDate = Date()
-    
-    @State var trackName : String = ""
-    @State var tracktext : String = ""
     let titlePlaceholder : String = "트랙 이름을 입력해 주세요"
     let textPlaceholder : String = "소개 글을 입력해 주세요"
     
     var body: some View {
-//        NavigationStack{
-            VStack{
-                HStack{
-                    VStack{
-                        Text("예상거리")
-                            .font(.footnote)
-                        Text("\(String(format: "%.2f",estimatedDistance))km")
-                            .italic()
-                            .fontWeight(.black)
-                    }
+        VStack(spacing: 20) {
+            // MARK: - 입력된 정보로 예상거리, 소요시간, 소모칼로리를 보여줌
+            HStack{
+                VStack{
+                    Text("예상거리")
+                        .font(.footnote)
+                    Text("\(String(format: "%.2f",estimatedDistance))km")
+                        .italic()
+                        .fontWeight(.black)
+                }
+                
+                Spacer()
+                
+                VStack{
+                    Text("소요시간")
+                        .font(.footnote)
+                    Text("\(estimatedTime)min")
+                        .italic()
+                        .fontWeight(.black)
+                }
+                
+                Spacer()
+                
+                VStack{
+                    Text("소모칼로리")
+                        .font(.footnote)
+                    Text("\(calories)kcal")
+                        .italic()
+                        .fontWeight(.black)
+                }
+            }
+            .foregroundStyle(.white)
+            .padding(.horizontal, 20)
+            
+            ScrollView(showsIndicators: false){
+                // MARK: - 트랙경로 설정하는 화면으로 이동
+                VStack{
+                    NavigationLink(destination: {
+                        // 마커 찍는 지도 들어갈 자리
+                        
+                    }, label: {
+                        ZStack(alignment: .center){
+                            Image("mapImage")
+                                .frame(minWidth: 300, maxWidth: .infinity,minHeight: 80,maxHeight: 80)
+                                .cornerRadius(10)
+                            Text("코스를 입력해 주세요")
+                                .fontWeight(.bold)
+                                .foregroundStyle(.black)
+                        }
+                    })
+                }.padding(.horizontal, 20)
+                
+                // MARK: - 트랙정보 입력폼
+                // 트랙이름
+                
+                TextField(text: $trackName) {
+                    Text("트랙 이름을 입력해주세요")
+                        .foregroundColor(.gray)
+                }
+                .foregroundColor(.white)
+                .padding(20)
+                
+                Divider().background(.gray)
+                
+                // 소개글
+                ZStack(alignment: .topLeading){
+                    TextEditor(text: $trackBio)
+                        .frame(maxWidth: .infinity, minHeight: 200)
+                        .scrollContentBackground(.hidden)
+                        .background(Color.main)
+                        .padding(20)
+                        .foregroundColor(.white)
                     
-                    Spacer()
-                    
-                    VStack{
-                        Text("소요시간")
-                            .font(.footnote)
-                        Text("\(estimatedTime)min")
-                            .italic()
-                            .fontWeight(.black)
-                    }
-                    
-                    Spacer()
-                    
-                    VStack{
-                        Text("소모칼로리")
-                            .font(.footnote)
-                        Text("\(calories)kcal")
-                            .italic()
-                            .fontWeight(.black)
+                    if trackBio.isEmpty{
+                        Text(textPlaceholder)
+                            .foregroundStyle(.gray)
+                            .padding(20)
                     }
                 }
-                .foregroundStyle(.white)
-                .padding(.horizontal, 20)
                 
-                ScrollView(showsIndicators: false){
-                VStack{
-                        NavigationLink(destination: {
-                            // 마커 찍는 지도 들어갈 자리
+                Divider().background(.gray)
+                
+                // MARK: - 인원, 날짜, 시간설정
+                VStack(spacing: 20) {
+                    // 예상 소요 시간
+                    HStack{
+                        Text("예상 소요 시간(분)")
+                        Spacer()
+                        Button(action: {
+                            minusTimeButtonTapped()
                         }, label: {
-                            ZStack(alignment: .center){
-                                Image("mapImage")
-//                                    .frame(width: .infinity, height: 80)
-                                    .frame(minWidth: 300, maxWidth: .infinity,minHeight: 80,maxHeight: 80)
-                                    .cornerRadius(10)
-                                Text("코스를 입력해 주세요")
-                                    .fontWeight(.bold)
-                                    .foregroundStyle(.black)
-                            }
+                            Image(systemName: "minus.circle.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.sub)
+                        })
+                        
+                        Text("\(estimatedTime)")
+                            .frame(width: 25)
+                            .fontWeight(.bold)
+                        
+                        Button(action: {
+                            plusTimeButtonTapped()
+                        }, label: {
+                            Image(systemName: "plus.circle.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.sub)
                         })
                     }
-                    
-                    ZStack(alignment: .topLeading){
-                        TextEditor(text: $trackName)
-                            .frame(maxWidth: .infinity, minHeight: 70)
-                            .scrollContentBackground(.hidden)
-                            .background(Color.main)
+                    .foregroundStyle(.white)
+                    .padding(.vertical, 10)
+                    HStack{ // 인원 설정
+                        Text("인원 설정")
                         
-                        if trackName.isEmpty{
-                            Text(titlePlaceholder)
-                                .foregroundStyle(.gray)
-                                .padding(.top, 10)
-                        }
-                    }
-                    
-                    Text("")
-                        .frame(minWidth: 10, maxWidth: .infinity, minHeight: 1, maxHeight: 1)
-                        .background(Color.gray)
-                    
-                    //            Divider()
-                    
-                    ZStack(alignment: .topLeading){
-                        TextEditor(text: $tracktext)
-                            .frame(maxWidth: .infinity, minHeight: 200)
-                            .scrollContentBackground(.hidden)
-                            .background(Color.main)
+                        Spacer()
                         
-                        if tracktext.isEmpty{
-                            Text(textPlaceholder)
-                                .foregroundStyle(.gray)
-                                .padding(.top, 10)
-                        }
-                    }
-                    
-                    
-                    Text("")
-                        .frame(minWidth: 10, maxWidth: .infinity, minHeight: 1, maxHeight: 1)
-                        .background(Color.gray)
-                    //            Divider()
-                    
-                    VStack{
-                        HStack{ // 예상 소요 시간
-                            Text("예상 소요 시간(분)")
-                            
-                            Spacer()
-                            
-                            Button(action: {
-                                if estimatedTime > 0 {
-                                    estimatedTime -= 1
-                                }
-                            }, label: {
-                                Image(systemName: "minus.circle.fill")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                    .foregroundStyle(.sub)
-                            })
-                            
-                            Text("\(estimatedTime)")
-                                .frame(width: 25)
-                                .fontWeight(.bold)
-                            
-                            Button(action: {
-                                if estimatedTime < 99 {
-                                    estimatedTime += 1
-                                }
-                            }, label: {
-                                Image(systemName: "plus.circle.fill")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                    .foregroundStyle(.sub)
-                            })
-                        }
-                        .foregroundStyle(.white)
-                        .padding(.vertical, 10)
-                        HStack{ // 인원 설정
-                            Text("인원 설정")
-                            
-                            Spacer()
-                            
-                            Button(action: {
-                                minus()
-                            }, label: {
-                                Image(systemName: "minus.circle.fill")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                    .foregroundStyle(.sub)
-                            })
-                            
-                            Text("\(numberOfPerson)").foregroundStyle(.white)
-                                .frame(width: 25)
-                                .fontWeight(.bold)
-                            
-                            Button(action: {
-                                plus()
-                            }, label: {
-                                Image(systemName: "plus.circle.fill")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                    .foregroundStyle(.sub)
-                            })
-                        }
-                        .foregroundStyle(.white)
-                        .padding(.vertical, 10)
-                        HStack{ // 날짜 설정
-                            Text("날짜 설정")
-                            
-                            Spacer()
-                            
-                            DatePicker("", selection: $runDate)
-                        }
-                        .foregroundStyle(.white)
-                        .padding(.vertical, 10)
-                    }
-                    .padding(.vertical)
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("코스 등록")
+                        Button(action: {
+                            minusMemberButtonTapped()
+                        }, label: {
+                            Image(systemName: "minus.circle.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.sub)
+                        })
+                        
+                        Text("\(limitedMember)").foregroundStyle(.white)
+                            .frame(width: 25)
                             .fontWeight(.bold)
-                            .foregroundStyle(.black)
-                            .padding(.horizontal, 120)
-                            .padding(.vertical, 15)
-                            .background(Color.sub)
-                            .cornerRadius(100)
-                    })
-                }
-//                .navigationTitle("코스 등록하기")
-//                .navigationBarTitleDisplayMode(.inline)
+                        
+                        Button(action: {
+                            plusMemberButtonTapped()
+                        }, label: {
+                            Image(systemName: "plus.circle.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.sub)
+                        })
+                    }
+                    .foregroundStyle(.white)
+                    .padding(.vertical, 10)
+                    HStack{ // 날짜 설정
+                        Text("날짜 설정")
+                        
+                        Spacer()
+                        
+                        DatePicker("", selection: $startDate)
+                    }
+                    .foregroundStyle(.white)
+                } .padding(.horizontal, 20)
             }
-            .padding(.horizontal, 20)
-            .background(Color.main)
-        
+            TUButton(text: "코스등록") {}
+                .padding(.horizontal, 20)
+                .padding(.bottom, 8)
+            
+        }
+        .onTapGesture {hideKeyboard()}
+        .background(Color.main)
     }
     
-    func plus() {
-        if numberOfPerson < 10 {
-            numberOfPerson += 1
+    // MARK: - Methods
+    func plusTimeButtonTapped() {
+        estimatedTime += 1
+    }
+    
+    func minusTimeButtonTapped() {
+        if estimatedTime > 0 {
+            estimatedTime -= 1
         }
     }
-    func minus() {
-        if numberOfPerson > 1 {
-            numberOfPerson -= 1
+    
+    func plusMemberButtonTapped() {
+        if limitedMember < 10 {
+            limitedMember += 1
         }
     }
+    
+    func minusMemberButtonTapped() {
+        if limitedMember > 1 {
+            limitedMember -= 1
+        }
+    }
+    
 }
 
 #Preview {
