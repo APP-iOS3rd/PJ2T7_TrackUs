58f9b2325d85491c343d0688737d9f9956247c30
SeonKoo <sungu09@naver.com>
2023-12-13 10:44:52 +0900

    fix: 진행중

--- TrackUs/TrackUs/Sources/Views/Mate/Recruitment.swift
+++ TrackUs/TrackUs/Sources/Views/Mate/Recruitment.swift
@@ -32,95 +32,95 @@ struct Recruitment: View {
     }
 }
 
-struct RecruitmentCell: View {
-    
-    let trackInfo: TrackInfo
-    
-    var body: some View {
-        NavigationLink(destination: MateDetailView(trackInfo: trackInfo)) {
-        VStack{
-            HStack(spacing: 10) {
-                
-                Spacer()
-                
-                Image(systemName: "figure.run")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("\(trackInfo.trackName)")
-                        .font(.headline)
-                        .bold()
-                        .lineLimit(1)
-                        .truncationMode(.tail)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .foregroundStyle(.white)
-                    HStack{
-                        Image(systemName: "figure.track.and.field")
-//                        Text("\(trackInfo.estimatedDistance)")
-                        if trackInfo.estimatedDistance < 1000 {
-                            Text(String(format: "%.0f m", trackInfo.estimatedDistance))
-                                .font(.subheadline)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundStyle(.sub)
-                        } else {
-                            Text(String(format: "%.1f km", trackInfo.estimatedDistance / 1000))
-                                .font(.subheadline)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundStyle(.sub)
-                        }
-                    }
-                    HStack{
-                        Image(systemName: "calendar")
-                        Text("\(formattedDate)")
-                            .font(.subheadline)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundStyle(.sub)
-                    }
-                    HStack{
-                        Image(systemName: "clock")
-                        Text("\(formattedDateTime)")
-                            .font(.subheadline)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundStyle(.sub)
-                    }
-                }
-                
-                Image(systemName: "chevron.forward")
-            }
-            
-            .padding(15)
-            
-            Text("")
-                .frame(minWidth: 10, maxWidth: .infinity, minHeight: 1, maxHeight: 1)
-                .background(Color.sub)
-            
-        }
-        .background(Color.main)
-        }
-    }
-    
-    var formattedDate: String {
-        let formatter  = DateFormatter()
-        formatter.dateFormat = "YYYY.MM.dd"
-        return formatter.string(from: trackInfo.startDate)
-    }
-    
-//    var formattedTime: String {
-//        let seconds = trackInfo.timeTaken
-//        let minutes = (seconds / 60) % 60
-//        let hours = seconds / 3600
-//        return String(format: "%02d:%02d", hours, minutes)
+//struct RecruitmentCell: View {
+//    
+//    let trackInfo: TrackInfo
+//    
+//    var body: some View {
+//        NavigationLink(destination: MateDetailView(trackInfo: trackInfo)) {
+//        VStack{
+//            HStack(spacing: 10) {
+//                
+//                Spacer()
+//                
+//                Image(systemName: "figure.run")
+//                    .resizable()
+//                    .frame(width: 30, height: 30)
+//                
+//                Spacer()
+//                
+//                VStack(alignment: .leading) {
+//                    Text("\(trackInfo.trackName)")
+//                        .font(.headline)
+//                        .bold()
+//                        .lineLimit(1)
+//                        .truncationMode(.tail)
+//                        .frame(maxWidth: .infinity, alignment: .leading)
+//                        .foregroundStyle(.white)
+//                    HStack{
+//                        Image(systemName: "figure.track.and.field")
+////                        Text("\(trackInfo.estimatedDistance)")
+//                        if trackInfo.estimatedDistance < 1000 {
+//                            Text(String(format: "%.0f m", trackInfo.estimatedDistance))
+//                                .font(.subheadline)
+//                                .frame(maxWidth: .infinity, alignment: .leading)
+//                                .foregroundStyle(.sub)
+//                        } else {
+//                            Text(String(format: "%.1f km", trackInfo.estimatedDistance / 1000))
+//                                .font(.subheadline)
+//                                .frame(maxWidth: .infinity, alignment: .leading)
+//                                .foregroundStyle(.sub)
+//                        }
+//                    }
+//                    HStack{
+//                        Image(systemName: "calendar")
+//                        Text("\(formattedDate)")
+//                            .font(.subheadline)
+//                            .frame(maxWidth: .infinity, alignment: .leading)
+//                            .foregroundStyle(.sub)
+//                    }
+//                    HStack{
+//                        Image(systemName: "clock")
+//                        Text("\(formattedDateTime)")
+//                            .font(.subheadline)
+//                            .frame(maxWidth: .infinity, alignment: .leading)
+//                            .foregroundStyle(.sub)
+//                    }
+//                }
+//                
+//                Image(systemName: "chevron.forward")
+//            }
+//            
+//            .padding(15)
+//            
+//            Text("")
+//                .frame(minWidth: 10, maxWidth: .infinity, minHeight: 1, maxHeight: 1)
+//                .background(Color.sub)
+//            
+//        }
+//        .background(Color.main)
+//        }
 //    }
-    
-    var formattedDateTime: String {
-        let formatter  = DateFormatter()
-        formatter.dateFormat = "h:mm a"
-        return formatter.string(from: trackInfo.startDate)
-    }
-}
+//    
+//    var formattedDate: String {
+//        let formatter  = DateFormatter()
+//        formatter.dateFormat = "YYYY.MM.dd"
+//        return formatter.string(from: trackInfo.startDate)
+//    }
+//    
+////    var formattedTime: String {
+////        let seconds = trackInfo.timeTaken
+////        let minutes = (seconds / 60) % 60
+////        let hours = seconds / 3600
+////        return String(format: "%02d:%02d", hours, minutes)
+////    }
+//    
+//    var formattedDateTime: String {
+//        let formatter  = DateFormatter()
+//        formatter.dateFormat = "h:mm a"
+//        return formatter.string(from: trackInfo.startDate)
+//    }
+//}
 
 //struct ListItemModel: Identifiable {
 //    let id = UUID()
